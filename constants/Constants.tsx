b823ad84fd1bfadd08172be4eb2dd39ef5788c41
Brian-Magomere-Igadwa <igadwabrian7@gmail.com>
2025-05-06 17:43:49 +0300

    feat:Add another version of LiveTranscribeScreen

--- constants/Constants.tsx
+++ constants/Constants.tsx
@@ -1,4 +1,6 @@
 export default {
-  API_BASE_URL: "http://localhost:8000",
-  WS_BASE_URL: "ws://localhost:8000",
+  // API_BASE_URL: "http://localhost:8000",
+  // WS_BASE_URL: "ws://localhost:8000",
+  API_BASE_URL: "http://10.0.2.2:8000",
+  WS_BASE_URL: "ws://10.0.2.2:8000",
 };
